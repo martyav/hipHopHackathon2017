6f7720aad52d190c5929c9043f273249f8f3c024
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-06-24 16:52:00 -0400

    Initial Commit

---
+++ EndPoints.swift
@@ -0,0 +1,16 @@
+//
+//  Endpoints.swift
+//  hiphopHackathon2017
+//
+//  Created by Marty Avedon on 6/24/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoints: String {
+    case Parks = "https://data.cityofnewyork.us/resource/4xyq-5bdm.json"
+    case Beaches = "https://www.nycgovparks.org/bigapps/DPR_Beaches_001.json"
+    case Events = "https://data.cityofnewyork.us/api/views/nyau-ykxp/rows.json?accessType=DOWNLOAD"
+}
+
---
+++ hipHopHackathon2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hipHopHackathon2017
+//
+//  Created by Marty Avedon on 6/24/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
