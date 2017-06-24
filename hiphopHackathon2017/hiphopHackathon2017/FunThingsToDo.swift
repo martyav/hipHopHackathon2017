23f842b82db46223c21b284aead7aa16237baa2b
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-06-24 16:06:08 -0400

    added some models to start

---
+++ hiphopHackathon2017/hiphopHackathon2017/FunThingsToDo.swift
@@ -0,0 +1,27 @@
+//
+//  FunThingsToDo.swift
+//  hiphopHackathon2017
+//
+//  Created by Marty Avedon on 6/24/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import Foundation
+
+class FunThingsToDo {
+    let name: String
+    let date: String
+    let location: String
+    
+    init(name: String, date: String, location: String) {
+        self.name = name
+        self.date = date
+        self.location = location
+    }
+}
+
+class Beach: FunThingsToDo {}
+
+class Park: FunThingsToDo {}
+
+class Event: FunThingsToDo {}
